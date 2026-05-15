6583395965f4990f8d1a1ad97e53a4279b08db06
Nolan <firesocietygames@gmail.com>
2026-05-15 16:58:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.ft_hangouts;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_messages) {
+                // load messages fragment
+                return true;
+            } else if (id == R.id.nav_call) {
+                // load call fragment
+                return true;
+            }
+            // etc.
+            return false;
+        });
+
+    }
+}
